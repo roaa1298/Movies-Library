038d6fc7b46ba54a575fdb71d190d46268429d6d
roaa1998 <roaabhailat4@gmail.com>
2022-02-16 23:02:56 +0200

    edit

--- schema.sql
+++ schema.sql
@@ -3,10 +3,8 @@ DROP TABLE IF EXISTS myMovie;
 CREATE TABLE IF NOT EXISTS myMovie (
     id SERIAL PRIMARY KEY ,
     title VARCHAR(255),
-    overview VARCHAR(10000),
+    overview text,
     release_date VARCHAR(255),
     poster_path VARCHAR(255),
-    video BOOLEAN,
-    vote_count INTEGER,
     comments VARCHAR(255)
 );
